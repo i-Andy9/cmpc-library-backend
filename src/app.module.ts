53aaab13228530d70471acabea487905bc8f4b0f
Andre <andre.zapata19@gmail.com>
2025-05-20 21:28:59 -0400

    feat: add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BookModule } from './book/book.module';
 @Module({
-  imports: [],
+  imports: [BookModule],
   controllers: [],
   providers: [],
 })
